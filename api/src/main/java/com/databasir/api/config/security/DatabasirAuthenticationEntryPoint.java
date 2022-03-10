6a8972bc2b9690e21b40a59600c48ad53d661d6a
vran <vran_dev@foxmail.com>
2022-03-10 11:56:30 +0800

    fix: response code error when unauthorized

--- api/src/main/java/com/databasir/api/config/security/DatabasirAuthenticationEntryPoint.java
+++ api/src/main/java/com/databasir/api/config/security/DatabasirAuthenticationEntryPoint.java
@@ -1,26 +1,39 @@
 package com.databasir.api.config.security;
 
+import com.databasir.common.JsonData;
+import com.databasir.core.domain.DomainErrors;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class DatabasirAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    private final ObjectMapper objectMapper;
+
     @Override
     public void commence(javax.servlet.http.HttpServletRequest request,
                          javax.servlet.http.HttpServletResponse response,
                          AuthenticationException authException) throws IOException, ServletException {
-        log.warn("验证未通过. 提示信息 - {} - {}", request.getRequestURI(), authException.getMessage());
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+        log.warn("验证未通过. 提示信息 - {} - {} {}", request.getRequestURI(),
+                authException.getClass().getName(),
+                authException.getMessage());
+
+        DomainErrors err = DomainErrors.INVALID_REFRESH_TOKEN_OPERATION;
+        JsonData<Void> data = JsonData.error(err.getErrCode(), err.getErrMessage());
+        String jsonString = objectMapper.writeValueAsString(data);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getOutputStream().write(jsonString.getBytes(StandardCharsets.UTF_8));
     }
 
 }
