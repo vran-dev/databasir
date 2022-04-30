6048b3937c7a79b17e32ad90566a2941484cb09d
vran <vran_dev@foxmail.com>
2022-04-30 22:52:12 +0800

    fix: checkstyle

--- core/src/main/java/com/databasir/core/domain/document/comparator/DocumentDiffs.java
+++ core/src/main/java/com/databasir/core/domain/document/comparator/DocumentDiffs.java
@@ -1,7 +1,6 @@
 package com.databasir.core.domain.document.comparator;
 
 import com.databasir.core.diff.data.DiffType;
-import com.databasir.core.domain.document.data.DatabaseDocumentSimpleResponse.TableData;
 import com.databasir.core.meta.data.*;
 import lombok.extern.slf4j.Slf4j;
 
