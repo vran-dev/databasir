92bf101da94461682bd11cdeca408fe970ea9082
vran <vran_dev@foxmail.com>
2022-05-18 19:18:23 +0800

    feat: optimize trigger provider design

--- meta/src/main/java/com/databasir/core/meta/provider/postgresql/PostgresqlTriggerMetaProvider.java
+++ meta/src/main/java/com/databasir/core/meta/provider/postgresql/PostgresqlTriggerMetaProvider.java
@@ -1,65 +1,25 @@
 package com.databasir.core.meta.provider.postgresql;
 
-import com.databasir.core.meta.data.TriggerMeta;
-import com.databasir.core.meta.provider.TriggerMetaProvider;
+import com.databasir.core.meta.provider.AbstractSqlTriggerMetaProvider;
 import com.databasir.core.meta.provider.condition.TableCondition;
 import lombok.extern.slf4j.Slf4j;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
-
 @Slf4j
-public class PostgresqlTriggerMetaProvider implements TriggerMetaProvider {
+public class PostgresqlTriggerMetaProvider extends AbstractSqlTriggerMetaProvider {
     @Override
-    public List<TriggerMeta> selectTriggers(Connection connection, TableCondition condition) {
-        String sql = "SELECT trigger_name         AS name,\n"
-                + "       action_timing        AS timing,\n"
-                + "       event_manipulation   AS manipulation,\n"
-                + "       action_statement     AS statement,\n"
-                + "       created              AS trigger_create_at,\n"
+    protected String sql(TableCondition condition) {
+        String sql = "SELECT trigger_name         AS TRIGGER_NAME,\n"
+                + "       action_timing        AS ACTION_TIMING,\n"
+                + "       event_manipulation   AS EVENT_MANIPULATION,\n"
+                + "       action_statement     AS ACTION_STATEMENT,\n"
+                + "       created              AS CREATED,\n"
                 + "       trigger_catalog      AS trigger_catalog,\n"
                 + "       TRIGGER_SCHEMA       AS trigger_schema,\n"
                 + "       event_object_catalog AS target_catalog,\n"
                 + "       event_object_schema  AS target_schema,\n"
                 + "       event_object_table   AS target_table_name\n"
                 + "FROM information_schema.triggers "
-                + "WHERE trigger_catalog = ? AND trigger_schema = ? AND event_object_table = ?;";
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-            preparedStatement.setObject(1, condition.getDatabaseName());
-            preparedStatement.setObject(2, condition.getSchemaName());
-            preparedStatement.setObject(3, condition.getTableName());
-            ResultSet results = preparedStatement.executeQuery();
-            List<TriggerMeta> triggers = new ArrayList<>();
-            while (results.next()) {
-                String name = Objects.requireNonNullElse(results.getString("name"), "");
-                String statement = results.getString("statement");
-                String timing = results.getString("timing");
-                String manipulation = results.getString("manipulation");
-                // postgresql default is null
-                String created = results.getString("trigger_create_at");
-                if (created == null) {
-                    created = "unknown";
-                }
-                TriggerMeta meta = TriggerMeta.builder()
-                        .name(name)
-                        .manipulation(manipulation)
-                        .timing(timing)
-                        .statement(statement)
-                        .createAt(created)
-                        .build();
-                triggers.add(meta);
-            }
-            return triggers;
-        } catch (SQLException e) {
-            log.warn("postgresql get trigger meta failed", e);
-            return Collections.emptyList();
-        }
+                + "WHERE trigger_catalog = '%s' AND trigger_schema = '%s' AND event_object_table = '%s';";
+        return String.format(sql, condition.getDatabaseName(), condition.getSchemaName(), condition.getTableName());
     }
 }
