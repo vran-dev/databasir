bf5c1b205fbfbcdee8e1ecf75381eab33bb762fa
vran <vran_dev@foxmail.com>
2022-06-11 12:45:42 +0800

    feat: optimize code

--- core/src/main/java/com/databasir/core/domain/discussion/data/DiscussionListCondition.java
+++ core/src/main/java/com/databasir/core/domain/discussion/data/DiscussionListCondition.java
@@ -15,20 +15,10 @@ public class DiscussionListCondition {
     @NotBlank
     private String tableName;
 
-    private String columnName;
-
     public Condition toCondition(Integer projectId) {
         List<Condition> conditions = new ArrayList<>();
         Condition condition = Tables.DOCUMENT_DISCUSSION.TABLE_NAME.eq(tableName);
         conditions.add(condition);
-
-        Condition columnCondition;
-        if (columnName != null) {
-            columnCondition = Tables.DOCUMENT_DISCUSSION.COLUMN_NAME.eq(columnName);
-        } else {
-            columnCondition = Tables.DOCUMENT_DISCUSSION.COLUMN_NAME.isNull();
-        }
-        conditions.add(columnCondition);
         conditions.add(Tables.DOCUMENT_DISCUSSION.PROJECT_ID.eq(projectId));
         return conditions.stream().reduce(Condition::and).orElse(DSL.trueCondition());
     }
